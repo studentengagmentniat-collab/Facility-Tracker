a91d7aae2c6acc531e9f719111a4e79cdc18b043
studentengagmentniat-collab <studentengagment.niat@gmail.com>
2026-04-16 13:41:36 +0530

    yes

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://gvudgmgeaxtzemirswuk.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2dWRnbWdlYXh0emVtaXJzd3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzMTk3NjIsImV4cCI6MjA5MTg5NTc2Mn0.0NqVQgIiV8DGX6FMjvYmgBMtVBuabOMpowvrc0Xb2bg';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
